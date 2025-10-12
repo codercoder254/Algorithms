f8ce7e59dbcda67e2208c524bd2196be6763b3d0
codercoder254 <codercoder254@gmail.com>
2025-10-13 00:06:46 +0300

    Initial commit

---
+++ LinearSearch.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+//function to ask input from user
+int* input()
+{
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    return arr;
+}
+
+//function to go through the array and find the target value
+int search(int arr[], int target)
+{
+    for (int i = 0; arr[i] != '\0'; i++)
+    {
+        if (arr[i] == target)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int* arr = input();
+    int target;
+    printf("Enter target element: ");
+    scanf("%d", &target);
+    int result = search(arr, target);
+    if (result != -1)
+    {
+        printf("Element found: %d\n", result);
+    }
+    else
+    {
+        printf("Element not found.\n");
+    }
+    
+    return 0;
+
+}
